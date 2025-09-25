d9b8fdcfabd0963ef73b88e4e11ff7273f243244
D4nyArt <dandavarteaga@gmail.com>
2025-09-24 23:29:38 -0400

    Mini Insta functionality complete (I think)

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProfileListView
+from .views import ProfileListView, ProfileDetailView
 
 urlpatterns = [
     path(r'', ProfileListView.as_view(), name='show_all_profiles'),
+    path(r'profile/<int:pk>', ProfileDetailView.as_view(), name="show_profile")
 ]
