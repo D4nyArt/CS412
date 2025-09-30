9030c9d9f25e15839f54accbcd3e64aeeb5a7789
D4nyArt <dandavarteaga@gmail.com>
2025-09-30 01:12:09 -0400

    Assignment 4 Initial commit

--- mini_insta/admin.py
+++ mini_insta/admin.py
@@ -7,7 +7,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile
+from .models import Profile, Post, Photo
 
 # Register the Profile model with the admin site
 admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Photo)
+
--- mini_insta/models.py
+++ mini_insta/models.py
@@ -27,3 +27,36 @@ class Profile(models.Model):
     def __str__(self):
         """Return string representation of the profile."""
         return f"{self.username}"
+    
+    def get_all_posts(self):
+        posts = Post.objects.filter(profile=self)
+        return posts
+    
+
+    
+class Post(models.Model):
+    
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now=True)
+    caption = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Post of profile: {self.profile}"
+    
+    def get_all_photos(self):
+        photos = Photo.objects.filter(post=self)
+        return photos
+    
+    def get_first_photo(self):
+        """Return the first Photo object related to this post, or None if none exist."""
+        photos = Photo.objects.filter(post=self)
+        return photos.first()
+    
+class Photo(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    image_url = models.URLField(blank=True)
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Photo of post: {self.post}"
+    
